038689c7329426282fefd015b6f0cf38559ad6a9
Momo23569 <yli3466@emory.edu>
2025-02-17 23:27:34 -0500

    Upload basic settings and requirements

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+class Config:
+    SECRET_KEY = os.environ.get("SECRET_KEY", "devkey")  
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        "DATABASE_URL", 
+        "postgresql://postgres:postgres@localhost:5432/my_movie_db"
+    )
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
